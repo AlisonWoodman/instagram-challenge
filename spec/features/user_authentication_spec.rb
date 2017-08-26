004de4bcf4437e0393295807c4368239c1a5bc3a
Alison Woodman <aliwoodman@gmail.com>
2017-08-26 12:31:04 +0100

    Users can sign up and sign out

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -4,19 +4,23 @@ RSpec.feature "User", type: :feature do
   let!(:user) { create(:user) }
 
   scenario "can sign up" do
-    visit('/users/sign_up')
-    fill_in 'user_email', with: 'user@gmail.com'
-    fill_in 'user_password', with: '123456abc.'
-    fill_in 'user_password_confirmation', with: '123456abc.'
-    click_button 'Sign up'
+    sign_up
     expect(page).to have_content('Welcome! You have signed up successfully.')
   end
 
-  # context "/sign_in" do
-  # scenario "After signing out, I'm forwarded to sign in" do
-  #   sign_up
-  #   click_button 'Sign out'
-  #   expect(current_path).to eq "/users/sign_in"
-  # end
-  # end
+  context "/sign_in" do
+  scenario "can sign out" do
+    sign_up
+    click_link 'Sign out'
+    expect(page).to have_content("Signed out successfully")
+  end
+  end
+
+  context "/sign_in" do
+  scenario "can sign out" do
+    sign_up
+    click_link 'Sign out'
+    expect(current_path).to eq("/users/sign_in")
+  end
+  end
 end
---
+++ spec/user_helper.rb
@@ -0,0 +1,7 @@
+def sign_up
+  visit('/users/sign_up')
+  fill_in 'user_email', with: 'user@gmail.com'
+  fill_in 'user_password', with: '123456abc.'
+  fill_in 'user_password_confirmation', with: '123456abc.'
+  click_button 'Sign up'
+end
