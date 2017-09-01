f6a18412d1753b49a1d491039a78cbfa0af65029
Alison Woodman <aliwoodman@gmail.com>
2017-09-01 14:28:36 +0100

    Development database configured for S3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,13 @@
 Rails.application.configure do
-# Settings specified here will take precedence over those in config/application.rb.
+  config.paperclip_defaults = {
+      storage: :s3,
+      s3_credentials: {
+        bucket: ENV.fetch('S3_BUCKET_NAME'),
+        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+        s3_region: ENV.fetch('AWS_REGION'),
+        access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      }
+    }# Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
